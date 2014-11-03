b6ddeccd06d332dd440d549381c47efe5d7466ab
Chris Schmitz <ccschmitz@gmail.com>
2014-11-03 06:47:20 -0600

    DevTrainified

--- lib/doc_page.rb
+++ lib/doc_page.rb
@@ -69,7 +69,7 @@ class DocPage < Html5Page
 
       div(class: "navbar-header cf title") {
         a(href: "/#{site_name}") {
-          span("RailsBridge ", class: "brand")
+          span("DevTrain ", class: "brand")
           text site_title
         }
       }
